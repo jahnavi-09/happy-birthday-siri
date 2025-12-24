044a5f5e7835a438bcad6804a01ed75b182a43b4
jahnavi-09 <70876993+jahnavi-09@users.noreply.github.com>
2025-12-24 20:54:44 +0530

    Update image paths in Gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,9 +11,9 @@ function Gallery({ isActive }) {
   const lightboxImgRef = useRef(null);
 
   const photos = [
-    { src: "public/images/pic2.jpg", alt: "Memory 1" },
-    { src: "public/images/pic2.jpg", alt: "Memory 2" },
-    { src: "public/images/pic3.jpg", alt: "Memory 3" },
+    { src: "/images/pic2.jpg", alt: "Memory 1" },
+    { src: "/images/pic2.jpg", alt: "Memory 2" },
+    { src: "/images/pic3.jpg", alt: "Memory 3" },
     { src: "/images/pic4.jpg", alt: "Memory 4" },
     { src: "/images/pic5.jpg", alt: "Memory 5" },
     { src: "/images/pic6.jpg", alt: "Memory 6" },
