85ed47e39eb2157e545492dce5871310f232ad00
jahnavi-09 <70876993+jahnavi-09@users.noreply.github.com>
2025-12-24 21:57:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/happy-birthday-siri/",
-  plugins: [react()],
+  base: '/happy-birthday-siri/',  // your repo name here
+  plugins: [react()]
 })
